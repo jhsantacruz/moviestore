2034127dac1a411ed7b344334b8615165bef9207
Juan Santa Cruz <jhsantacruz@icloud.com>
2014-10-16 21:55:54 -0700

    Added devise along with the views for login/register

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
   root 'movies#index'
   resources :movies, only: [:show, :index]
 end
