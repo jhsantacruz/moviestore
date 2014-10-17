a5be7ec461b9d836f7790565a00a353370f1897b
Juan Santa Cruz <jhsantacruz@icloud.com>
2014-10-16 23:28:49 -0700

    Added cart feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  
+  get 'carts/show'
+
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  
-  root 'movies#index'
   resources :movies, only: [:show, :index]
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
+
+  root 'movies#index'
+
 end
